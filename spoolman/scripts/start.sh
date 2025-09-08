42a57127793e3077b4d2e7894ad2198973201863
TopdRob <TopdRob@users.noreply.github.com>
2025-09-08 17:55:21 +0200

    Update start.sh

--- spoolman/scripts/start.sh
+++ spoolman/scripts/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bashio
+#!/usr/bin/env bash
 
 # ANSI color codes
 ORANGE='\033[0;33m'
@@ -7,16 +7,15 @@ NC='\033[0m' # No Color
 echo -e "${ORANGE}Updating environment${NC}"
 
 # Configure Spoolman to listen on the Ingress host and port
-# The Supervisor provides these values via environment variables.
 export SPOOLMAN_HOST=$(bashio::addon.ingress_host)
 export SPOOLMAN_PORT=$(bashio::addon.ingress_port)
 export SPOOLMAN_BASE_PATH=$(bashio::addon.ingress_entry)
 
 # Set other Spoolman environment variables
 export SPOOLMAN_DB_TYPE=sqlite
-export SPOOLMAN_DIR_DATA=/config
-export SPOOLMAN_DIR_BACKUPS=/config/backups
-export SPOOLMAN_DIR_LOGS=/config
+export SPOOLMAN_DIR_DATA=/config/spoolman
+export SPOOLMAN_DIR_BACKUPS=/config/spoolman/backups
+export SPOOLMAN_DIR_LOGS=/config/spoolman
 
 # User-configurable options
 SPOOLMAN_DEBUG_MODE=$(bashio::config 'debug_mode')
@@ -26,5 +25,8 @@ export SPOOLMAN_LOGGING_LEVEL
 SPOOLMAN_AUTOMATIC_BACKUP=$(bashio::config 'auto_backup')
 export SPOOLMAN_AUTOMATIC_BACKUP
 
-# Start the Spoolman application
-/var/spoolman/scripts/start.sh
+# Start the Spoolman application directly
+# Dit roept het originele start.sh script van Spoolman aan in de uitgepakte map
+echo -e "${ORANGE}Starting Spoolman using original start script...${NC}"
+cd /var/spoolman
+./scripts/start.sh
